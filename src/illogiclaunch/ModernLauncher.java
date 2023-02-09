8ef5003faf9b802788b0753bff856815c00683ae
altrisi <altrisi.trillosierra@gmail.com>
2023-02-09 20:17:57 +0100

    Fully working installation, add error handling to launcher

--- src/illogiclaunch/ModernLauncher.java
+++ src/illogiclaunch/ModernLauncher.java
@@ -1,19 +1,43 @@
 package illogiclaunch;
 
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+
+import java.awt.GraphicsEnvironment;
 import java.io.*;
+
+import javax.swing.JOptionPane;
 
 import com.formdev.flatlaf.FlatLightLaf;
 
 public class ModernLauncher {
-	public static void main(String[] args) throws IOException, ReflectiveOperationException {
+	public static final String MAIN_FILE_PATH = "/illogicmainclass.txt";
+	public static void main(String[] args) throws IOException {
 		FlatLightLaf.setup();
-		try (InputStream is = ModernLauncher.class.getResourceAsStream("illogicmainclass.txt")) {
-			String clazz = new BufferedReader(
-				      new InputStreamReader(is))
+		String mainClass;
+		try (InputStream is = ModernLauncher.class.getResourceAsStream(MAIN_FILE_PATH)) {
+			if (is == null) {
+				fail("Launch info not found");
+				throw new IllegalStateException("Launch info not found!");
+			}
+			mainClass = new BufferedReader(new InputStreamReader(is))
 				        .lines()
 				        .findFirst().get();
-			Class.forName(clazz).getDeclaredMethod("main", String[].class).invoke(null, new Object[] {args});
-			
+		} catch (IOException e) {
+			fail("Exception reading launch info");
+			throw new UncheckedIOException(e);
 		}
+		System.out.println("About to launch modernified app with class " + mainClass);
+		try {
+			Class.forName(mainClass).getDeclaredMethod("main", String[].class).invoke(null, new Object[] { args });
+		} catch (ReflectiveOperationException e) {
+			fail("Exception finding or invoking main class");
+			throw new IllegalStateException(e);
+		}
+		
+	}
+	private static void fail(String reason) {
+		System.err.println("Failed to launch! " + reason);
+		if (!GraphicsEnvironment.isHeadless())
+			JOptionPane.showMessageDialog(null, "Failed to launch! " + reason, "Failed to launch", ERROR_MESSAGE);
 	}
 }
--- src/illogicworks/modernifier/Installation.java
+++ src/illogicworks/modernifier/Installation.java
@@ -1,15 +1,20 @@
 package illogicworks.modernifier;
 
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
-
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.jar.Manifest;
 import java.util.stream.Stream;
 
+import illogiclaunch.ModernLauncher;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class Installation {
+	private static final String MF_PATH = "META-INF/MANIFEST.MF";
+	private static final String LAUNCHER_CLASS = ModernLauncher.class.getName();
 	private static final String DEV_PATH = "bin/built.jar";
 	private static final boolean DEV_ENV = true;
 
@@ -24,22 +29,29 @@ public class Installation {
 		}
 		FileSystem us = FileSystems.newFileSystem(ourJar, new HashMap<>(), null);
 		FileSystem target = FileSystems.newFileSystem(targetJar, new HashMap<>(), null);
+
+		if (Files.exists(target.getPath(ModernLauncher.MAIN_FILE_PATH))) {
+			us.close();
+			target.close();
+			throw new IllegalArgumentException("Target is already modernified!");
+		}
 		
 		Path temp = Files.createTempDirectory("modernifier-temp");
 		
 		for (Path root : us.getRootDirectories()) {
 			for (Path p : iter(Files.walk(root)
-					.filter(p -> !p.toString().startsWith("illogicworks")))) {
-				if (p.toString().equals("/")) continue;
+					.filter(p -> !p.toString().startsWith("/illogicworks") // don't copy installer
+								&& !p.toString().equals("/") // special case, idk what is this
+								&& !p.toString().equals('/' + MF_PATH) // we special-case the manifest later
+								&& !p.toString().equals("/module-info.class") // don't rename to flatlaf
+					))) {
 				System.out.println("Temping " + p);
 				Path tempFile = temp.resolve(p.toString().substring(1));
 				Files.copy(p, tempFile, REPLACE_EXISTING);
 			}
 		}
-		try (InputStream is = Files.newInputStream(temp.resolve("META-INF"))) {
-			Manifest manifest = new Manifest(is);
-			// TODO read and put main class for launcher
-		}
+
+		handleManifest(target, temp);
 		
 		for (Path p : iter(Files.walk(temp))) {
 			if (Files.isDirectory(p)) continue;
@@ -54,10 +66,30 @@ public class Installation {
 		target.close();
 		System.out.println("DID Stuff, temp at " + temp);
 	}
-	static URI zipURI(URI path) throws URISyntaxException {
+
+	private static void handleManifest(FileSystem target, Path base) throws IOException {
+		Manifest manifest;
+		try (InputStream is = Files.newInputStream(target.getPath(MF_PATH))) {
+			manifest = new Manifest(is);
+		}
+		
+		String originalMainClass = manifest.getMainAttributes().getValue("Main-Class");
+		Files.write(base.resolve(ModernLauncher.MAIN_FILE_PATH.substring(1)), Collections.singleton(originalMainClass)); // Java 8 :(
+		
+		manifest.getMainAttributes().putValue("Main-Class", LAUNCHER_CLASS);
+		manifest.getMainAttributes().putValue("Multi-Release", "true"); // flatlaf has some J9+ specifics
+		// TODO check if we need to merge more things
+		
+		try (OutputStream out = Files.newOutputStream(base.resolve(MF_PATH))) {
+			manifest.write(out);
+		}
+	}
+
+	private static URI zipURI(URI path) throws URISyntaxException {
 		return URI.create("jar:" + path);
 	}
-	static Iterable<Path> iter(Stream<Path> s) {
+
+	private static Iterable<Path> iter(Stream<Path> s) {
 		return () -> s.iterator();
 	}
 }
